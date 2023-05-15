93a35917dcf8972bbd8485daebdac9c1f8410aab
sediri-code <63328434+sediri-code@users.noreply.github.com>
2023-05-15 23:10:38 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [customerInfo, setCustomerInfo] = useState({});
+  
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    // Send customer info to your email here using your preferred method
+    console.log(customerInfo);
+  };
+
+  const articles = [
+    {
+      id: 1,
+      name: 'Article 1',
+      price: 9.99,
+      image: 'https://placeimg.com/640/480/animals'
+    },
+    {
+      id: 2,
+      name: 'Article 2',
+      price: 14.99,
+      image: 'https://placeimg.com/640/480/nature'
+    },
+    // Add 10 more articles here
+  ];
+
+  const [selectedArticle, setSelectedArticle] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Yasmina Shop</h1>
+      <div className="article-container">
+        {articles.map((article) => (
+          <div key={article.id} className="article">
+            <img src={article.image} alt={article.name} />
+            <div className="article-info">
+              <h3>{article.name}</h3>
+              <p>${article.price.toFixed(2)}</p>
+              <button onClick={() => setSelectedArticle(article)}>Buy Now</button>
+            </div>
+          </div>
+        ))}
+      </div>
+      {selectedArticle && (
+        <form onSubmit={handleFormSubmit}>
+          <h2>{selectedArticle.name}</h2>
+          <img src={selectedArticle.image} alt={selectedArticle.name} />
+          <p>${selectedArticle.price.toFixed(2)}</p>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" onChange={(e) => setCustomerInfo({ ...customerInfo, name: e.target.value })} />
+          <label htmlFor="address">Address</label>
+          <input type="text" id="address" onChange={(e) => setCustomerInfo({ ...customerInfo, address: e.target.value })} />
+          <label htmlFor="phone">Phone Number</label>
+          <input type="text" id="phone" onChange={(e) => setCustomerInfo({ ...customerInfo, phone: e.target.value })} />
+          <button type="submit">Submit</button>
+        </form>
+      )}
     </div>
   );
 }
